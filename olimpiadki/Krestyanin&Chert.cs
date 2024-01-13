27fb50f5bb33b4ebe5d8a1d220f4eb63601d20f9
VavilenGI <145273223+VavilenGI@users.noreply.github.com>
2024-01-14 01:40:08 +0600

    fix Krestyanin&Chert.cs

--- olimpiadki/Krestyanin&Chert.cs
+++ olimpiadki/Krestyanin&Chert.cs
@@ -4,11 +4,10 @@ class Program
     static void Main()
     {
         double MaxN, a=0;
-        MaxN = Convert.ToInt64(Console.ReadLine());
+        MaxN = Convert.ToDouble(Console.ReadLine());
         for (double i = 1; i <= 1+Math.Log2(MaxN +1);i++)
         {
             a += Math.Floor(MaxN / (Math.Pow(2, i) - 1));
-            Console.WriteLine(a);
         }
         Console.WriteLine(a);
     }
